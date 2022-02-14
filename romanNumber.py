cf770620eaccefbdb4f873fc94634566ae5ae830
Alicia Amador <alicia.amador.cabezali@gmail.com>
2022-02-14 23:42:30 +0100

    Añado funciones de comparación y primera version de funciones aritmeticas

--- romanNumber.py
+++ romanNumber.py
@@ -14,4 +14,84 @@ class RomanNumber(object):
         else:
             raise TypeError("Tipo introducido incorrecto")
        
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self.romano
+
+    def __int__(self):
+        return self.arabigo
+
+    #Funcion mayor que
+    def __gt__(self,valor):
+        if not isinstance(valor, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return self.arabigo > valor.arabigo
+        
+            
+        
+    #Funcion menor que
+    def __lt__(self,RN):
+        if isinstance(RN, RomanNumber):
+            return self.arabigo < RN.arabigo
+        elif isinstance(RN, int or float):
+            return self.arabigo < RN
+
+    #Otras funciones de comparacion
+    # x<=y
+    def __le__(self,RN):
+        if not isinstance(RN, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return self.arabigo <= RN.arabigo
+
+    # x==y
+    def __eq__(self,RN):
+        if not isinstance(RN, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return self.arabigo == RN.arabigo
+    
+    # x!=y
+    def __ne__(self,RN):
+        if not isinstance(RN, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return self.arabigo != RN.arabigo
+
+    # x>=y
+    def __ge__(self,RN):
+        if not isinstance(RN, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return self.arabigo >= RN.arabigo
+
+    #Funcion suma
+    def __add__(self, RN):
+        if isinstance(RN, RomanNumber):
+            return RomanNumber(self.arabigo + RN.arabigo)
+        elif isinstance(RN, int ):
+            return RomanNumber(self.arabigo + RN)
+    def __radd__(self,other):
+        if isinstance(other, RomanNumber):
+            return RomanNumber(self.arabigo + other.arabigo)
+        elif isinstance(other, int):
+            return RomanNumber(self.arabigo + other)
+       
+
+    #Funcion resta
+    def __sub__(self, RN):
+        if not isinstance(RN, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return RomanNumber(self.arabigo - RN.arabigo)
+
+    #Funcion multiplicacion
+    def __mul__(self,RN):
+        if not isinstance(RN, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return RomanNumber(self.arabigo * RN.arabigo)
+
+    #Funcion division
+    def __truediv__(self,RN):
+        if not isinstance(RN, RomanNumber):
+            raise TypeError("Tiene que ser un numero romano")
+        return RomanNumber(self.arabigo//RN.arabigo)
+
     
